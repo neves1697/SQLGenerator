6837419595a7c2581dca46865b571152edd59d44
Anderson Neves <anderson.neves@aliare.co>
2024-12-24 09:43:22 -0300

    feature: Adicionado um container de divs interno na tela de início para ser responsável por gerenciar as informações visíveis em tela

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import "../Styles/Inicio.scss";
 
 const Inicio = () => {
-    return <>
+    return (
         <div className="container-home">
             <div className="container-interno">
                 <div className="textos">
                     <h1>SQL Generator</h1>
-                    <p>SQL Generator é uma aplicação que ajuda a montar SQLs para executar
-                        nos clientes. Traz algumas consultas prontas e alguns recursos
-                        experimentais
+                    <p>
+                        SQL Generator é uma aplicação que ajuda a montar SQLs para
+                        executar nos clientes. Traz algumas consultas prontas e alguns
+                        recursos experimentais
                     </p>
+                </div>
+
+                <div className="informacoes">
+                    <div className="consultas">
+                        teste
+                    </div>
+                    <div className="consultas">
+                        teste
+                    </div>
                 </div>
             </div>
         </div>
-    </>
-}
+    );
+};
 
 export default Inicio;
