9ac3ad421ffab26971a7e29287bd51a7ecba5ea7
Anderson Neves <neves1697@gmail.com>
2025-01-13 23:23:04 -0300

    feat: Adicionando cor no modal e colocando um ícone de (CLOSE) no modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import "../Styles/Modal.scss";
+import { AiTwotoneCloseSquare } from "react-icons/ai";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+
 
 const Modal = ({ isOpen, onClose, children }) => {
   // Se o modal não estiver aberto, não renderiza nada
   if (!isOpen) return null;
 
-  return (
+  return <>
     <div className="modal-overlay">
       <div className="modal-content">
-        <button className="close-button" onClick={onClose}>X</button>
+        <button className="close-button" onClick={onClose}>
+          <IoMdCloseCircleOutline size={30} color='white' />
+        </button>
         {children} {/* Exibe o conteúdo passado para o Modal */}
       </div>
     </div>
-  );
+  </>
 };
 
 export default Modal;
